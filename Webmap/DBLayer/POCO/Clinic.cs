fd3c3b93ab3a29a6d7906cfb73c303c089b0adb2
Abhijit BAvdhankar <abhijitbavdhankar@gmail.com>
2017-02-26 20:09:56 -0500

    test

--- Webmap/DBLayer/POCO/Clinic.cs
+++ Webmap/DBLayer/POCO/Clinic.cs
@@ -1,11 +1,15 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Webmap.DBLayer.POCO {
 
     //Represents a single clinic information
     public class Clinic {
-        public DateTime Timings { get; set; }
-        public Object ContactInfo { get; set; }
-        public string URL { get; set; }
+        public string ClinicID { get; set; }
+        public string ClinicName { get; set; }
+        public string ClinicTimings { get; set; }
+        public Object ClinicContactInfo { get; set; } //Email,Telephone 
+        public string ClinicWebSiteURL { get; set; }
+        public List<Doctor> DoctorVisitingClinics { get; set; } 
     }
 }
---
+++ Webmap/DBLayer/POCO/Doctor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Webmap.DBLayer.POCO {
+
+    //Represents a single clinic information
+    public class Doctor {
+        public DateTime DoctorAppointmentTimings { get; set; }
+        public DateTime DoctorWorkingTimings { get; set; }
+        public int DoctorID { get; set; }
+        public string DoctorName { get; set; }
+        public Object DoctorContactInfo { get; set; } //Email,Telephone
+        public string UserBasedRatings { get; set; }
+    }
+}
