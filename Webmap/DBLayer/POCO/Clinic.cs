094edf052c0f1bd49860f32e0b0e088d340ea8de
OwaisQureshi <owaiskureshi@gmail.com>
2017-02-26 19:42:56 -0500

    -added db class for DB instance -Poco folder and class for representing DB objects -Installed LiteDB

---
+++ Webmap/DBLayer/POCO/Clinic.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Webmap.DBLayer.POCO {
+
+    //Represents a single clinic information
+    public class Clinic {
+        public DateTime Timings { get; set; }
+        public Object ContactInfo { get; set; }
+        public string URL { get; set; }
+    }
+}
---
+++ Webmap/DBLayer/db.cs
@@ -0,0 +1,13 @@
+﻿using System.Configuration;
+using LiteDB;
+
+namespace Webmap.DBLayer {
+    public class DB {
+        string dbString = ConfigurationManager.AppSettings["dbString"];//prop
+        LiteDatabase dbInstance;
+
+        public DB() {
+            dbInstance = new LiteDatabase(dbString);
+        }
+    }
+}
