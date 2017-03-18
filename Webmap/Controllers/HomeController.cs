865719b3bff147770ac48a7d1ad4870197809007
OwaisQureshi <owaiskureshi@gmail.com>
2017-03-18 17:31:14 -0400

    Revert "Email Added"
    This reverts commit 0fde7f352882442b7c0a5bf203cf3dd179a2c6b8.

--- Webmap/Controllers/HomeController.cs
+++ Webmap/Controllers/HomeController.cs
@@ -3,12 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using System.Data;
-using System.Data.Entity;
-using System.Threading.Tasks;
-using System.Net;
-using System.Net.Mail;
-using Webmap.Models;
 
 namespace Webmap.Controllers
 {
@@ -43,58 +37,6 @@ namespace Webmap.Controllers
         public ActionResult MyHtml(string htmlPageName)
         {
             var result = new FilePathResult($"~/Views/{htmlPageName}.html", "text/html");
-            return result;
-        }
-
-        public ActionResult Email()
-        {
-            try
-            {
-                SmtpClient smtp = new SmtpClient("smtp.gmail.com", 587);
-                smtp.UseDefaultCredentials = false;
-                smtp.Credentials = new NetworkCredential("patientnowait", "kangaroo1234");
-                smtp.EnableSsl = true;
-                smtp.Send("patientnowait@gmail.com", "abhijitbavdhankar@gmail.com", "Subject - Test Mail", "Email Body - Test Mail");               
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
-            var result = new FilePathResult("~/Views/Home/Hospital.html", "text/html");
-            return result;
-        }
-
-        //public ActionResult Email(string patientname, string patientemail, string doctorname, string doctoremail, string appdate, string apptime)
-        //public ActionResult Emails(string patientemail)
-        [HttpPost]
-        public ActionResult Emails(FormCollection form)
-        {
-            try
-            {
-                SmtpClient smtp = new SmtpClient("smtp.gmail.com", 587);
-                smtp.UseDefaultCredentials = false;
-                smtp.Credentials = new NetworkCredential("patientnowait", "kangaroo1234");
-                smtp.EnableSsl = true;                                
-                //smtp.Send("patientnowait@gmail.com", patientemail, "Subject - Test Mail", "Email Body - Test Mail" + patientname);
-
-                string patientname = Convert.ToString(Request["txtPatientName"].ToString());
-                string patientemail = Convert.ToString(Request["txtPatientEmailAddress"].ToString());
-                String doctorname = Convert.ToString(Request["txtDoctorName"].ToString());
-                String appdate = Convert.ToString(Request["txtAppointementDate"].ToString());
-                String apptime = Convert.ToString(Request["txtAppointementTime"].ToString());
-
-                smtp.Send("patientnowait@gmail.com", patientemail, "Subject - Test Mail", "Email Body - Test Mail");
-
-                //var data2 = $('#txt2').val();
-                //var strConfirmAppointemnt = "Dear {0} Your Appointement with Dr. {1} has been confirmed on date {3} and time {4}.";
-                //var strFinalMessage = String.Format(strConfirmAppointemnt, patientname, doctorname, appdate, apptime);
-                //smtp.Send("patientnowait@gmail.com", patientemail, "Subject - Appointment Confirmation", strConfirmAppointemnt);
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
-            var result = new FilePathResult("~/Views/Home/Hospital.html", "text/html");
             return result;
         }
     }
