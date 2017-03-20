2044cee0891c422384857b30ac7fbc83329bafe6
abhijit <abhijit@DELL>
2017-03-19 20:20:03 -0400

    GUI Modal window and Email added

--- Webmap/Controllers/HomeController.cs
+++ Webmap/Controllers/HomeController.cs
@@ -3,6 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data;
+using System.Data.Entity;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Mail;
+using Webmap.Models;
 
 namespace Webmap.Controllers
 {
@@ -37,6 +43,24 @@ namespace Webmap.Controllers
         public ActionResult MyHtml(string htmlPageName)
         {
             var result = new FilePathResult($"~/Views/{htmlPageName}.html", "text/html");
+            return result;
+        }
+
+        public ActionResult Email()
+        {
+            try
+            {
+                SmtpClient smtp = new SmtpClient("smtp.gmail.com", 587);
+                smtp.UseDefaultCredentials = false;
+                smtp.Credentials = new NetworkCredential("patientnowait", "kangaroo1234");
+                smtp.EnableSsl = true;
+                smtp.Send("patientnowait@gmail.com", "abhijitbavdhankar@gmail.com", "Subject - Pateint No Wait Mail", "Email Body - Pateint No WaitTest Mail");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            var result = new FilePathResult("~/Views/Home/Hospital.html", "text/html");
             return result;
         }
     }
